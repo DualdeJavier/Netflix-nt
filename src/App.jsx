7c2716d2659d45684949af29058c2e9aa35a79ff
Dualde <javier.dualde@hotmail.com>
2023-04-13 13:02:36 -0300

    Fixing pages and adding more

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import ListadoPeliculas from './Views/ListadoPeliculas';
 import Celebrity01 from './Views/Celebrity01';
 import Celebrity02 from './Views/Celebrity02';
 import Blockbuster from './Views/BlockBuster';
+import CelebrityInfo from './Views/CelebrityInfo';
+import CelebritySingle from './Views/CelebritySingle';
 
 export default function App() {
   return (
@@ -15,6 +17,8 @@ export default function App() {
       <Route path="Blockbuster" element={<Blockbuster />} />
       <Route path="Celebrity01" element={<Celebrity01 />} />
       <Route path="Celebrity02" element={<Celebrity02 />} />
+      <Route path="celebrity-info" element={<CelebrityInfo />} />
+      <Route path="celebritySingle" element={<CelebritySingle />} />
     </Routes>
   </BrowserRouter>
   );
--- src/Views/PageWrapper2.jsx
+++ src/Views/PageWrapper2.jsx
@@ -15,14 +15,14 @@ export default function PageWrapper2(props) {
             <h3>Login</h3>
             <form method="post" action="#">
               <div className="row">
-                <label for="username">
+                <label htmlFor="username">
                   Username:
                   <input type="text" name="username" id="username" placeholder="Hugh Jackman" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$" required="required" />
                 </label>
               </div>
   
               <div className="row">
-                <label for="password">
+                <label htmlFor="password">
                   Password:
                   <input type="password" name="password" id="password" placeholder="******" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
                 </label>
@@ -55,26 +55,26 @@ export default function PageWrapper2(props) {
             <h3>sign up</h3>
             <form method="post" action="#">
               <div className="row">
-                <label for="username-2">
+                <label htmlFor="username-2">
                   Username:
                   <input type="text" name="username" id="username-2" placeholder="Hugh Jackman" pattern="^[a-zA-Z][a-zA-Z0-9-_\.]{8,20}$" required="required" />
                 </label>
               </div>
   
               <div className="row">
-                <label for="email-2">
+                <label htmlFor="email-2">
                   your email:
                   <input type="password" name="email" id="email-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
                 </label>
               </div>
               <div className="row">
-                <label for="password-2">
+                <label htmlFor="password-2">
                   Password:
                   <input type="password" name="password" id="password-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
                 </label>
               </div>
               <div className="row">
-                <label for="repassword-2">
+                <label htmlFor="repassword-2">
                   re-type Password:
                   <input type="password" name="password" id="repassword-2" placeholder="" pattern="(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$" required="required" />
                 </label>
@@ -121,8 +121,8 @@ export default function PageWrapper2(props) {
                     <ul className="dropdown-menu level1">
                       <li><a href="celebrity01">CELEBRITY LISTING </a></li>
                       <li><a href="celebrity02">CELEBRITY LISTING - GRID </a></li>
-                      <li><a href="celebritylist.html">celebrity list</a></li>
-                      <li className="it-last"><a href="celebritysingle.html">celebrity single</a></li>
+                      <li><a href="celebrity-info">celebrity info</a></li>
+                      <li className="it-last"><a href="celebritySingle">celebrity single</a></li>
                     </ul>
                   </li>
                   <li className="dropdown first">
